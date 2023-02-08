a5c463eb1f3037c7ad49ebc716b1e978d47a1214
Maximiliano <maxi.lozon@gmail.com>
2023-02-07 23:15:26 -0300

    responsive header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -8,15 +8,22 @@ const Header = () => {
   return (
     <header>
       <div className="container header__container">
-        <h5>Hello i am</h5>
-        <h1>Maxi Lozon</h1>
-        <h5 className="tex-ligth">Developer</h5>
-        <CTA/>
-        <HeaderSocials/>
-        <div className="me">
-          <img src={ME} alt="me" />
+      
+        <div className='grid__content'>
+          <div className='izq'>
+          <h5>Hello i am</h5>
+          <h1>Maxi Lozon</h1>
+          <h5 className="tex-ligth">Developer</h5>
+          <CTA/>
+          </div>
+          <div className='der'>
+            <div className="me">
+              <img src={ME} alt="me" />
+            </div>
+          </div>
         </div>
         
+        <HeaderSocials/>
       </div>
     </header>
   )
